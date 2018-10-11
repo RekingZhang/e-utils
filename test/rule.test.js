fb7406ca68f505504e9f248c700239b1a787918f
zhangkaiqiang <zhangkaiqiang@58.com>
2018-10-11 17:11:15 +0800

    feat(validator): 新增校验工具类

---
+++ test/rule.test.js
@@ -0,0 +1,29 @@
+import rule from '../lib/rule';
+
+describe('rule', () => {
+	test('测试校验邮箱', () => {
+		expect(rule.isEmail('121212@')).not.toBeTruthy();
+		expect(rule.isEmail('121212@58')).not.toBeTruthy();
+		expect(rule.isEmail('121212@5.com')).not.toBeTruthy();
+		expect(rule.isEmail('121212@gamil.com')).toBeTruthy();
+	});
+	test('测试校验手机号', () => {
+		expect(rule.isMobilePhone('121212')).not.toBeTruthy();
+		expect(rule.isMobilePhone('18513833315')).toBeTruthy();
+	});
+	test('测试校验身份证', () => {
+		//长度校验
+		expect(rule.isCreditCard('121212')).not.toBeTruthy();
+		//地区码校验
+		expect(rule.isCreditCard('160105194912310029')).not.toBeTruthy();
+		//生日校验
+		expect(rule.isCreditCard('110105194902310026')).not.toBeTruthy();
+		//校验码校验
+		expect(rule.isCreditCard('110105194912310021')).not.toBeTruthy();
+		//15为身份证校验
+		expect(rule.isCreditCard('110105491331002')).not.toBeTruthy();
+		expect(rule.isCreditCard('110105491231002')).toBeTruthy();
+		//150122199010194636
+		expect(rule.isCreditCard('11010519491231002X')).toBeTruthy();
+	});
+});
--- test/validator.test.js
+++ test/validator.test.js
@@ -1,29 +1,42 @@
-import validator from './../lib/validator';
+import Validator from '../lib/validator';
+
 
 describe('validator', () => {
-	test('测试校验邮箱', () => {
-		expect(validator.isEmail('121212@')).not.toBeTruthy();
-		expect(validator.isEmail('121212@58')).not.toBeTruthy();
-		expect(validator.isEmail('121212@5.com')).not.toBeTruthy();
-		expect(validator.isEmail('121212@gamil.com')).toBeTruthy();
+	test('测试正则校验', () => {
+		let validator = new Validator([{
+				rule: /^\S/,
+				message: '不能为空'
+			},
+			{
+				rule: /\d/,
+				message: '必须是数字'
+			}
+		])
+		expect(validator.check(10)).toBe(true);
+		expect(validator.getMessage()).toBe('');
+		expect(validator.check('')).toBe(false);
+		expect(validator.getMessage()).toBe('不能为空');
+		expect(validator.check('ss')).toBe(false);
+		expect(validator.getMessage()).toBe('必须是数字');
 	});
-	test('测试校验手机号', () => {
-		expect(validator.isMobilePhone('121212')).not.toBeTruthy();
-		expect(validator.isMobilePhone('18513833315')).toBeTruthy();
+	test('测试添加校验', () => {
+		let validator = new Validator()
+		validator.addRule({
+			rule: /\d/,
+			message: '必须是数字'
+		});
+		expect(validator.check('ss')).toBe(false);
+		expect(validator.getMessage()).toBe('必须是数字');
 	});
-	test('测试校验身份证', () => {
-		//长度校验
-		expect(validator.isCreditCard('121212')).not.toBeTruthy();
-		//地区码校验
-		expect(validator.isCreditCard('160105194912310029')).not.toBeTruthy();
-		//生日校验
-		expect(validator.isCreditCard('110105194902310026')).not.toBeTruthy();
-		//校验码校验
-		expect(validator.isCreditCard('110105194912310021')).not.toBeTruthy();
-		//15为身份证校验
-		expect(validator.isCreditCard('110105491331002')).not.toBeTruthy();
-		expect(validator.isCreditCard('110105491231002')).toBeTruthy();
-		//150122199010194636
-		expect(validator.isCreditCard('11010519491231002X')).toBeTruthy();
-	});
+	test('测试自定义校验函数', () => {
+		let validator = new Validator({
+			rule: function (val) {
+				return /^\S/.test(val);
+			},
+			message: '不能为空'
+		})
+		expect(validator.check('')).toBe(false);
+		expect(validator.check('ss')).toBe(true);
+	})
+
 });
