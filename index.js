15a5af436d5d3848b8e865edf7d062733a3f4258
zhangkaiqiang <zhangkaiqiang@58.com>
2018-10-23 15:18:47 +0800

    fix(index): 新增导出rule工具方法

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ import math from './lib/math';
 import singleton from './lib/singleton';
 import throttle from './lib/throttle';
 import type from './lib/type';
+import rule from './lib/rule';
 import validator from './lib/validator';
 
-export { Cookie, debounce, getter, math, singleton, throttle, type, validator };
+export {
+	Cookie,
+	debounce,
+	getter,
+	math,
+	singleton,
+	throttle,
+	type,
+	rule,
+	validator
+};
