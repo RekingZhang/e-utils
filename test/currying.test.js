1f14c545b13a61f2dc542d1f9d42166a44c724c9
RekingZhang <724255238@qq.com>
2018-12-24 15:55:37 +0800

    perf(currying): 改进currying方法

--- test/currying.test.js
+++ test/currying.test.js
@@ -1,11 +1,15 @@
 import currying from './../lib/currying';
 
 describe('currying', () => {
-	function add(n1, n2) {
-		return n1 + n2;
+	function add(a, b, c, d) {
+		return a + b + c + d;
 	}
 	test('柯里化', () => {
 		let addCurry = currying(add, 1);
-		expect(addCurry(4)).toBe(5);
+		expect(addCurry(2, 3)(4)).toBe(10);
+	});
+	test('柯里化', () => {
+		let addCurry = currying(add);
+		expect(addCurry(1, 2)(3)(4)).toBe(10);
 	});
 });
